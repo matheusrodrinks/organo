ddf7afc28041fa76ff031a5bb9cf5be1be83c467
MATHEUS RODRIGUES MENDONÇA <82005382+matheusrodrinks@users.noreply.github.com>
2023-02-16 14:02:46 -0400

    labels e inputs

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+const CampoTexto = (props) => {
+    
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label> {/* retorna os valores no app, precisa das chaves. */}
+            <input placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default CampoTexto
