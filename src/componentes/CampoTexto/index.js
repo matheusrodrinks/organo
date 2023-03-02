a7f1adb6538457029c66bfebcf7dc5070733511f
MATHEUS RODRIGUES MENDONÇA <82005382+matheusrodrinks@users.noreply.github.com>
2023-03-02 15:54:54 -0400

    botao, props, evento e preventDefault

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,19 @@
 import './CampoTexto.css'
 const CampoTexto = (props) => {
-    
+
+    const placeholderModificada = `${props.placeholder}...`
+
+    let valor = 'Neymar Junior'
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label> {/* retorna os valores no app, precisa das chaves. */}
-            <input placeholder={props.placeholder}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
