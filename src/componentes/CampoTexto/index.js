c62704f24f36bdd589008d47c66295ffcc49e08c
MATHEUS RODRIGUES MENDONÇA <82005382+matheusrodrinks@users.noreply.github.com>
2023-03-06 20:08:35 -0400

    adicionando usestates

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,17 +3,16 @@ const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...`
 
-    let valor = 'Neymar Junior'
+    
 
     const aoDigitado = (evento) => {
-        valor = evento.target.value
-        console.log(evento.target.value)
+        props.aoAlterado(evento.target.value)
     }
 
     return (
         <div className="campo-texto">
             <label>{props.label}</label> {/* retorna os valores no app, precisa das chaves. */}
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
